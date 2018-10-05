27e96cb01e4bef579b5c56659459163a360c9ffa
koda <kdkyung57@gmail.com>
2018-10-06 07:02:02 +0900

    test: Add test code

--- src/main/java/com/sode/phonebook/Controller/WebRestController.java
+++ src/main/java/com/sode/phonebook/Controller/WebRestController.java
@@ -1,4 +1,4 @@
-package com.sode.phonebook.Controller;
+package com.sode.phonebook.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
