5f6d82c35cee221e39d6ebfd6e0da957b1475b11
Dakyung Ko <kdkyung57@gmail.com>
2018-10-06 06:31:17 +0900

    init commit

---
+++ src/main/java/com/sode/phonebook/Controller/WebRestController.java
@@ -0,0 +1,13 @@
+package com.sode.phonebook.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "HelloWorld";
+	}
+}
