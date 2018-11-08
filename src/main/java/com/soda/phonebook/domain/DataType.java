9e2de311ebe0e25bf222b23a9a5ba58294c60c3b
koda <kdkyung57@gmail.com>
2018-11-09 06:34:04 +0900

    fix: error

--- src/main/java/com/soda/phonebook/domain/DataType.java
+++ src/main/java/com/soda/phonebook/domain/DataType.java
@@ -1,13 +1,13 @@
 package com.soda.phonebook.domain;
 
 import javax.persistence.Column;
-import javax.persistence.Convert;
+//import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import com.soda.phonebook.converter.TypeAttributeConverter;
+//import com.soda.phonebook.converter.TypeAttributeConverter;
 import com.soda.phonebook.domain.VO.Type;
 
 import lombok.AccessLevel;
@@ -23,7 +23,7 @@ public class DataType extends BaseEntity{
 	@Column(name="name", nullable=false)
 	private String name;
 	
-	@Convert(converter = TypeAttributeConverter.class)
+	//@Convert(converter = TypeAttributeConverter.class)
 	@Column(name="type", nullable=false)
 	private Type type; // default 값 설정해야 
 	
