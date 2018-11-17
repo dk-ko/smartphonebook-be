402d15958a370b30b57b895f58c40e872561af18
koda <kdkyung57@gmail.com>
2018-11-17 15:39:14 +0900

    feat: Edit Contact list read

---
+++ src/main/java/com/soda/phonebook/dto/res/ContactListReadResponseDto.java
@@ -0,0 +1,32 @@
+package com.soda.phonebook.dto.res;
+
+import java.util.List;
+import java.util.Set;
+
+import com.soda.phonebook.domain.Contact;
+import com.soda.phonebook.domain.Digit;
+import com.soda.phonebook.domain.Tag;
+import com.soda.phonebook.domain.info.Info;
+
+import lombok.Getter;
+
+@Getter
+public class ContactListReadResponseDto {
+	private Long id;
+	private String name;
+	private String memo;
+	private byte[] photo;
+	private List<Digit> digits;
+	private List<Info> infoes;
+	private Set<Tag> tags;
+	
+	public ContactListReadResponseDto(Contact contact) {
+		this.id = contact.getId();
+		this.name = contact.getName();
+		this.memo = contact.getMemo();
+		this.photo = contact.getPhoto();
+		this.digits = contact.getDigits();
+		this.infoes = contact.getInfoes();
+		this.tags = contact.getTags();
+	}
+}
