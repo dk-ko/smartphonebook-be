c95868cbee97bed9ed5e8574b6f4068c71ca4954
koda <kdkyung57@gmail.com>
2018-12-04 22:10:56 +0900

    test: Domain Model

--- src/test/java/com/soda/phonebook/domain/ContactTest.java
+++ src/test/java/com/soda/phonebook/domain/ContactTest.java
@@ -15,11 +15,11 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.soda.phonebook.domain.VO.ContactType;
+import com.soda.phonebook.domain.VO.DataType;
+import com.soda.phonebook.domain.VO.Numbers;
+import com.soda.phonebook.domain.info.Address;
 import com.soda.phonebook.repository.UserRepository;
 
-//import lombok.extern.slf4j.Slf4j;
-
-//@Slf4j
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class ContactTest {
@@ -32,6 +32,8 @@ public class ContactTest {
 	
 	private Contact contact;
 	private User user, savedUser;
+	private Numbers numbers = Numbers.builder()
+			.first("010").second("1234").third("5678").build();
 	
 	@Before
 	public void setUp() {
@@ -48,26 +50,52 @@ public class ContactTest {
 	@Test
 	@Transactional
 	public void test_Contact_빌더패턴_생성() {
-		
-//		log.info(contact.toString());
-		
 		assertThat(contact.getId(), is(nullValue())); // JPA ID 생성 규약 
 		assertThat(contact.getName(), is("koda"));
 	}
-	/*
+	
 	@Test
 	@Transactional
-	public void test_Contact_cascade_저장() {
+	public void test_Contact_cascade_저장및삭제() {
 		
 		// digit, info, tag 생성 후 add		
+		Category category1 = Category.builder()
+				.user(savedUser)
+				.type(DataType.DIGIT)
+				.name("개인").build();
+		Category category2 = Category.builder()
+				.user(savedUser)
+				.type(DataType.ADDRESS)
+				.name("집").build();
+		Digit digit = Digit.builder()
+				.numbers(numbers)
+				.category(category1)
+				.contact(contact)
+				.build();
+		Address address = Address.builder()
+				.contents("서울시 구로구 ...")
+				.category(category2)
+				.contact(contact)
+				.build();
+		Tag tag = Tag.builder().name("태그저장").user(savedUser).build();
+		
+		contact.getDigits().add(digit);
+		contact.getInfoes().add(address);
+		contact.getTags().add(tag);
+		
 		// contact만 persist
+		em.persist(contact);
+		em.flush();
 		
-		assertThat(contact.getId(), is(1l));
+		assertNotNull(em.find(Digit.class, 1l));
+		assertNotNull(em.find(Address.class, 1l));
+		assertNotNull(em.find(Tag.class, 1l));
 		
-		// is not empty
-		assertFalse(contact.getDigits().isEmpty());
-		assertFalse(contact.getInfoes().isEmpty());
-		assertFalse(contact.getTags().isEmpty()); 
+		em.remove(contact);
+		em.flush();
 		
-	}*/
+		// 삭제 후 null
+		assertNull(em.find(Digit.class, 1l));
+		assertNull(em.find(Address.class, 1l));
+	}
 }
--- src/test/java/com/soda/phonebook/domain/UserTest.java
+++ src/test/java/com/soda/phonebook/domain/UserTest.java
@@ -1,9 +1,13 @@
 package com.soda.phonebook.domain;
 
 import com.soda.phonebook.domain.User;
+import com.soda.phonebook.domain.VO.DataType;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -18,14 +22,53 @@ import com.soda.phonebook.repository.UserRepository;
 @DataJpaTest
 public class UserTest {
 
+	@PersistenceContext
+    private EntityManager em;
+	
 	@Autowired
 	private UserRepository userRepository;
 	
-	private User user;
+	private User user, sUser;
+	private Contact contact;
+	private Tag tag;
+	private Category category1, category2;
 	
 	@Before
 	public void setUp() throws Exception{
+		// test 1
 		user = User.builder().name("user1").build();
+		
+		// test 2
+		// user - contact, tag, category, favorite
+		sUser = userRepository.save(user);
+		
+		tag = Tag.builder().name("태그저장").user(sUser).build();
+		em.persist(tag);
+		
+		category1 = Category.builder()
+				.user(sUser)
+				.type(DataType.DIGIT)
+				.name("개인").build();
+		category2 = Category.builder()
+				.user(sUser)
+				.type(DataType.ADDRESS)
+				.name("집").build();
+		em.persist(category1);
+		em.persist(category2);
+		
+		contact = Contact.builder()
+				.name("테스트연락처")
+				.user(sUser).build();
+		em.persist(contact);
+		
+		sUser.getContacts().add(contact);
+		sUser.getTags().add(tag);
+		sUser.getCategories().add(category1);
+		sUser.getCategories().add(category2);
+		sUser.getFavorites().add(contact);
+		
+		em.persist(sUser);
+		em.flush();
 	}
 	
 	@Test
@@ -36,5 +79,22 @@ public class UserTest {
 		assertThat(savedUser.getName(), is("user1"));
 		assertThat(findedUser.getName(), is("user1"));
 		
+	}
+	
+	@Test
+	public void test_User삭제시_Cascade적용() {
+		// user - contact, tag, category, favorite
+		assertNotNull(em.find(Category.class, 1l));
+		assertNotNull(em.find(Category.class, 2l));
+		assertNotNull(em.find(Contact.class, 1l));
+		assertNotNull(em.find(Tag.class, 1l));
+		
+		em.remove(sUser);
+		em.flush();
+		
+		assertNull(em.find(Category.class, 1l));
+		assertNull(em.find(Category.class, 2l));
+		assertNull(em.find(Contact.class, 1l));
+		assertNull(em.find(Tag.class, 1l));
 	}
 }
