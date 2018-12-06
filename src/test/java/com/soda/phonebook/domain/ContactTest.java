895998d6d5f3f4cdbbab5821935b0f6dacf98dcb
koda <kdkyung57@gmail.com>
2018-12-06 21:55:57 +0900

    fix: build test error

--- src/test/java/com/soda/phonebook/domain/ContactTest.java
+++ src/test/java/com/soda/phonebook/domain/ContactTest.java
@@ -1,6 +1,5 @@
 package com.soda.phonebook.domain;
 
-import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import javax.persistence.EntityManager;
@@ -24,45 +23,32 @@ import com.soda.phonebook.repository.UserRepository;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class ContactTest {
-	
+
 	@PersistenceContext
-    private EntityManager em;
-	
+	private EntityManager em;
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private TagRepository tagRepository;
-	
+
 	private Contact contact;
 	private User user, savedUser;
-	private Numbers numbers = Numbers.builder()
-			.first("010").second("1234").third("5678").build();
-	
+	private Numbers numbers = Numbers.builder().first("010").second("1234").third("5678").build();
+
 	@Before
 	public void setUp() {
-		
+
 		user = User.builder().name("testuser").build();
 		savedUser = userRepository.save(user);
-		
+
 		contact = Contact.builder()
 				.user(savedUser)
 				.type(ContactType.DEFAULT)
 				.name("koda").build();
-	}
-	
-	@Test
-	@Transactional
-	public void test_Contact_빌더패턴_생성() {
-		assertThat(contact.getId(), is(nullValue())); // JPA ID 생성 규약 
-		assertThat(contact.getName(), is("koda"));
-	}
-	
-	@Test
-	@Transactional
-	public void test_Contact_cascade_저장및삭제() {
-		
-		// digit, info, tag 생성 후 add		
+
+		// digit, info, tag 생성 후 add
 		Category category1 = Category.builder()
 				.user(savedUser)
 				.type(DataType.DIGIT)
@@ -74,34 +60,41 @@ public class ContactTest {
 		Digit digit = Digit.builder()
 				.numbers(numbers)
 				.category(category1)
-				.contact(contact)
-				.build();
+				.contact(contact).build();
 		Address address = Address.builder()
 				.contents("서울시 구로구 ...")
 				.category(category2)
-				.contact(contact)
-				.build();
-		Tag tag = Tag.builder().name("태그저장").user(savedUser).build();
-		
+				.contact(contact).build();
+		Tag tag = Tag.builder()
+				.name("태그저장")
+				.user(savedUser).build();
+
 		contact.getDigits().add(digit);
 		contact.getInfoes().add(address);
 		contact.getTags().add(tag);
-		
+
 		// contact만 persist
 		em.persist(contact);
 		em.flush();
-		
+	}
+
+	@Test
+	@Transactional
+	public void test_Contact_cascade_저장() {
+
 		assertNotNull(em.find(Digit.class, 1l));
 		assertNotNull(em.find(Address.class, 1l));
 		assertNotNull(tagRepository.findById(1l));
-		
+	}
+
+	public void test_Contact_cascade_삭제() {
 		em.remove(contact);
 		em.flush();
-		
+
 		// 삭제 후 null
 		assertNull(em.find(Digit.class, 1l));
 		assertNull(em.find(Address.class, 1l));
-		
+
 		em.clear();
 	}
 }
