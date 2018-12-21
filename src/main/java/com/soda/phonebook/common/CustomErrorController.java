215a6f76ded3195c773079ee9fa0ed1644ada957
koda <kdkyung57@gmail.com>
2018-12-21 22:46:27 +0900

    fix: Delete error controller

--- src/main/java/com/soda/phonebook/common/CustomErrorController.java
+++ src/main/java/com/soda/phonebook/common/CustomErrorController.java
@@ -1,36 +1,37 @@
-package com.soda.phonebook.common;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@AllArgsConstructor
-@Controller
-public class CustomErrorController implements ErrorController{
-	
-	
-	@RequestMapping("/error")
-	public <T extends Exception> String returnMain(HttpServletRequest request) {
-		
-		Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
-		Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
-		
-		log.info("[CustomErrorController]");
-		log.error("[statusCode]: "+statusCode+", [exception]: "+exception);
-		
-		return "redirect:/hello";
-//		return "hello";
-	}
-
-	@Override
-	public String getErrorPath() {
-		return "/error";
-	}
-	
-}
+//package com.soda.phonebook.common;
+//
+//import javax.servlet.http.HttpServletRequest;
+//
+//import org.springframework.boot.web.servlet.error.ErrorController;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//
+//@Slf4j
+//@AllArgsConstructor
+//@Controller
+//public class CustomErrorController implements ErrorController{
+//	
+//	
+//	@RequestMapping("/error")
+//	public <T extends Exception> String returnMain(HttpServletRequest request) {
+//		
+//		Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+//		Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
+//		
+//		log.info("[CustomErrorController]");
+//		log.error("[statusCode]: "+statusCode+", [exception]: "+exception);
+//		
+//		return "redirect:/hello";
+////		return "hello";
+//	}
+//
+//	@Override
+//	public String getErrorPath() {
+//		return "/error";
+//	}
+//	
+//}
