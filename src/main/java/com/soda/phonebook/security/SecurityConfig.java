e1c571384db9158f85e8a4de15f20b83c2cd3019
koda <kdkyung57@gmail.com>
2018-12-23 18:19:02 +0900

    fix: Build error

--- src/main/java/com/soda/phonebook/security/SecurityConfig.java
+++ src/main/java/com/soda/phonebook/security/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.soda.phonebook.security;
 import javax.servlet.Filter;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -23,15 +25,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
     		http.antMatcher("/**")
-        
-        .authorizeRequests()
-        		.antMatchers("/", "/h2/**", "/favicon.ico", "/login**")
+    		.sessionManagement()
+    			.sessionCreationPolicy(SessionCreationPolicy.NEVER)
+        .and().authorizeRequests()
+        		.antMatchers("/", "/h2/**", "/favicon.ico", "/api/login**")
+        			.permitAll()
+        		.antMatchers(HttpMethod.OPTIONS, "/**")
         			.permitAll()
         		.anyRequest()
                 	.authenticated()
         
         .and().logout()
-        		.logoutRequestMatcher(new AntPathRequestMatcher("/logout_processing"))
+        		.logoutRequestMatcher(new AntPathRequestMatcher("/api/logout_processing"))
         		.logoutSuccessUrl("/")
         			.permitAll()
         		.invalidateHttpSession(true)
